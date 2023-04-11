3ac8fb40e32aee9e18ba74fe7657387864b4bb5b
sirisha517 <sai.sirifriends8790@gmail.com>
2023-04-11 13:10:36 +0530

    app

--- main.tf
+++ main.tf
@@ -24,6 +24,7 @@ resource "aws_launch_template" "main" {
 }
 
 resource "aws_autoscaling_group" "main" {
+  name                  = "${var.component}-${var.env}"
   desired_capacity      = var.desired_capacity
   max_size              = var.max_size
   min_size              = var.min_size
