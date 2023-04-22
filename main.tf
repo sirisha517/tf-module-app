0febcd3967e82d6a5b4d25fcc8be603a598a8cf9
sirisha517 <sai.sirifriends8790@gmail.com>
2023-04-22 15:58:08 +0530

    open application port

--- main.tf
+++ main.tf
@@ -1,21 +1,16 @@
 resource "aws_launch_template" "main" {
   name = "${var.component}-${var.env}"
-
   iam_instance_profile {
     name = aws_iam_instance_profile.main.name
   }
-
   image_id = data.aws_ami.ami.id
   instance_market_options {
     market_type = "spot"
   }
-
   instance_type          = var.instance_type
   vpc_security_group_ids = [aws_security_group.main.id]
-
   tag_specifications {
     resource_type = "instance"
-
     tags = merge(
       var.tags,
       { Name = "${var.component}-${var.env}" }
@@ -26,7 +21,6 @@ resource "aws_launch_template" "main" {
     env       = var.env
   }))
 }
-
 resource "aws_autoscaling_group" "main" {
   name                = "${var.component}-${var.env}"
   desired_capacity    = var.desired_capacity
@@ -40,7 +34,7 @@ resource "aws_autoscaling_group" "main" {
   }
   tag {
     key                 = "Name"
-    propagate_at_launch = true
+    propagate_at_launch = false
     value               = "${var.component}-${var.env}"
   }
 }
@@ -48,7 +42,6 @@ resource "aws_security_group" "main" {
   name        = "${var.component}-${var.env}"
   description = "${var.component}-${var.env}"
   vpc_id      = var.vpc_id
-
   ingress {
     description = "SSH"
     from_port   = 22
@@ -56,7 +49,6 @@ resource "aws_security_group" "main" {
     protocol    = "tcp"
     cidr_blocks = var.bastion_cidr
   }
-
   ingress {
     description = "APP"
     from_port   = var.port
@@ -64,7 +56,6 @@ resource "aws_security_group" "main" {
     protocol    = "tcp"
     cidr_blocks = var.allow_app_to
   }
-
   egress {
     from_port        = 0
     to_port          = 0
@@ -72,7 +63,6 @@ resource "aws_security_group" "main" {
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
   }
-
   tags = merge(
     var.tags,
     { Name = "${var.component}-${var.env}" }
@@ -83,28 +73,22 @@ resource "aws_lb_target_group" "main" {
   port     = var.port
   protocol = "HTTP"
   vpc_id   = var.vpc_id
-
   health_check {
-    enabled = true
-    healthy_threshold = 2
-    unhealthy_threshold = 2
-    timeout = 4
-    interval = 5
+    enabled             = true
+    healthy_threshold   = 2
+    unhealthy_threshold = 5
+    interval            = 5
+    timeout             = 4
   }
   tags = merge(
     var.tags,
     { Name = "${var.component}-${var.env}" }
   )
 }
-
-resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
-}
-
 resource "aws_route53_record" "main" {
-  zone_id = data.aws_route53_zone.domain.id
+  zone_id = data.aws_route53_zone.domain.zone_id
   name    = local.dns_name
   type    = "CNAME"
   ttl     = 30
-  records = [var.dns_domain]
+  records = [var.alb_dbs_name]
 }
