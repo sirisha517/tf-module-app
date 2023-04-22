652a6bc180b3c85ea418320d11aa76816b7ab04d
sirisha517 <sai.sirifriends8790@gmail.com>
2023-04-22 10:55:59 +0530

    open application port

--- main.tf
+++ main.tf
@@ -33,6 +33,7 @@ resource "aws_autoscaling_group" "main" {
   max_size            = var.max_size
   min_size            = var.min_size
   vpc_zone_identifier = var.subnets
+  target_group_arns   = [aws_lb_target_group.main.arn]
   launch_template {
     id      = aws_launch_template.main.id
     version = "$Latest"
@@ -76,4 +77,26 @@ resource "aws_security_group" "main" {
     var.tags,
     { Name = "${var.component}-${var.env}" }
   )
+}
+resource "aws_lb_target_group" "main" {
+  name     = "${var.component}-${var.env}"
+  port     = var.port
+  protocol = "HTTP"
+  vpc_id   = var.vpc_id
+
+  health_check {
+    enabled = true
+    healthy_threshold = 2
+    unhealthy_threshold = 2
+    timeout = 4
+    interval = 5
+  }
+  tags = merge(
+    var.tags,
+    { Name = "${var.component}-${var.env}" }
+  )
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
 }
