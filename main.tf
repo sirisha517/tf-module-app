fc6c3789d8596b24e6361a90a3eeedc826c14ee8
sirisha517 <sai.sirifriends8790@gmail.com>
2023-04-24 14:41:32 +0530

    open application port

--- main.tf
+++ main.tf
@@ -1,26 +1,33 @@
 resource "aws_launch_template" "main" {
   name = "${var.component}-${var.env}"
+
   iam_instance_profile {
     name = aws_iam_instance_profile.main.name
   }
+
   image_id = data.aws_ami.ami.id
   instance_market_options {
     market_type = "spot"
   }
+
   instance_type          = var.instance_type
   vpc_security_group_ids = [aws_security_group.main.id]
+
   tag_specifications {
     resource_type = "instance"
+
     tags = merge(
       var.tags,
       { Name = "${var.component}-${var.env}" }
     )
   }
+
   user_data = base64encode(templatefile("${path.module}/userdata.sh", {
     component = var.component
     env       = var.env
   }))
 }
+
 resource "aws_autoscaling_group" "main" {
   name                = "${var.component}-${var.env}"
   desired_capacity    = var.desired_capacity
@@ -44,6 +51,7 @@ resource "aws_security_group" "main" {
   name        = "${var.component}-${var.env}"
   description = "${var.component}-${var.env}"
   vpc_id      = var.vpc_id
+
   ingress {
     description = "SSH"
     from_port   = 22
@@ -51,6 +59,7 @@ resource "aws_security_group" "main" {
     protocol    = "tcp"
     cidr_blocks = var.bastion_cidr
   }
+
   ingress {
     description = "APP"
     from_port   = var.port
@@ -65,11 +74,13 @@ resource "aws_security_group" "main" {
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
   }
+
   tags = merge(
     var.tags,
     { Name = "${var.component}-${var.env}" }
   )
 }
+
 resource "aws_lb_target_group" "main" {
   name     = "${var.component}-${var.env}"
   port     = var.port
