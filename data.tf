6d4fe818e5699cbd1dc05e41faed211836ca63c6
sirisha517 <sai.sirifriends8790@gmail.com>
2023-05-03 22:34:36 +0530

    fix shipping

--- data.tf
+++ data.tf
@@ -1,6 +1,6 @@
 data "aws_ami" "ami" {
   most_recent      = true
-  name_regex       = "devops-prac-with-ansible" //AMI NAME
+  name_regex       = "devops-practice-with-ansible" //AMI NAME
   owners           = ["self"] //OWNER
 }
 
