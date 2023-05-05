4062fcb9731831692e4ac3857c9efa2d6d7d3214
sirisha517 <sai.sirifriends8790@gmail.com>
2023-05-06 04:28:27 +0530

    payment fix

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ resource "aws_launch_template" "main" {
 
     tags = merge(
       var.tags,
-      { Name = "${var.component}-${var.env}" }
+      { Name = "${var.component}-${var.env}" ,Monitor = "yes"}
     )
   }
 
